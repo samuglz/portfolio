988ecbd2da75f77f467856ca2a7a4a675429e4fd
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-03-15 23:26:19 +0000

    Add BottomMenu Left Items

--- src/components/BottomMenu.vue
+++ src/components/BottomMenu.vue
@@ -1,13 +1,48 @@
 <template>
     <div
-        class="w-full bg-dark-background flex justify-between items-center absolute bottom-0 border-solid border-black text-menu-color text-sm py-1"
+        class="w-full bg-dark-background flex justify-between items-center absolute bottom-0 border-t border-solid border-black text-menu-color text-sm"
     >
-        <div class="flex justify-start w-4/12 items-center px-2">
-            <div>Left Items</div>
+        <div class="flex justify-start w-60 items-center">
+            <div class="bg-blue-500 px-3">
+                <img
+                    class="w-3 h-6"
+                    src="../assets/img/Remote_Icon.svg"
+                    alt="remote icon"
+                />
+            </div>
+            <div class="flex px-2 items-center">
+                <img
+                    class="w-5 h-5"
+                    src="../assets/img/Git_Icon.svg"
+                    alt="Git Icon"
+                />
+                <span class="px-1">main</span>
+            </div>
+            <div class="flex mx-3">
+                <img
+                    class="w-4 h-5"
+                    src="../assets/img/Sync_Icon.svg"
+                    alt="Sync Icon"
+                />
+            </div>
+            <div class="flex items-center">
+                <img
+                    class="w-4 h-5"
+                    src="../assets/img/Times_Icon.svg"
+                    alt="Times Icon"
+                />
+                <span class="px-2">0</span>
+            </div>
+            <div class="flex items-center">
+                <img
+                    class="w-4 h-5"
+                    src="../assets/img/Danger_Icon.svg"
+                    alt="Danger Icon"
+                />
+                <span class="px-2">0</span>
+            </div>
         </div>
-        <div class="flex justify-end w-8/12 items-center px-2">
-            <div>Right Items</div>
-        </div>
+        <div class="flex justify-end w-8/12 items-center px-2"></div>
     </div>
 </template>
 
