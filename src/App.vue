4ba236cb01ddaa1d8b6a1133bb3838ad331ca1b2
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-03-16 00:10:29 +0000

    Added CenterScreen Component 🔥

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="w-screen h-screen bg-background">
         <TopMenu />
+        <CenterScreen />
         <BottomMenu />
     </div>
 </template>
@@ -8,8 +9,9 @@
 <script setup>
 import TopMenu from './components/TopMenu.vue';
 import BottomMenu from './components/BottomMenu.vue';
+import CenterScreen from './components/CenterScreen.vue';
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
-<style></style>
+<style scoped></style>
---
+++ src/components/CenterScreen.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-total w-full flex">
+        <LeftMenu />
+    </div>
+</template>
+
+<script setup>
+import LeftMenu from './LeftMenu.vue';
+</script>
+
+<style scoped>
+.h-total {
+    height: 93.2%;
+}
+</style>
