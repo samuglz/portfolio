47ab8141ac6485c29352fb6217d47762cc1be28d
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-03-16 16:26:14 +0000

    created CodeSection Component

--- src/components/CenterScreen.vue
+++ src/components/CenterScreen.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="h-total w-full flex">
         <LeftMenu />
+        <CodeSection />
     </div>
 </template>
 
 <script setup>
 import LeftMenu from './LeftMenu.vue';
+import CodeSection from './CodeSection.vue';
 </script>
 
 <style scoped>
---
+++ src/components/CodeSection.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-full h-full bg-dark-background"></div>
+</template>
+
+<script setup></script>
+
+<style></style>
