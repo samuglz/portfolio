48c53358dc114691b99b351674d0ce321a51fc1a
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-03-18 14:26:26 +0000

    added i18n support 📋

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,19 @@
     </div>
 </template>
 
-<script setup>
+<script>
 import TopMenu from './components/TopMenu.vue';
 import BottomMenu from './components/BottomMenu.vue';
 import CenterScreen from './components/CenterScreen.vue';
+export default {
+    name: 'App',
+    components: {
+        TopMenu,
+        BottomMenu,
+        CenterScreen
+    },
+    setup() {}
+};
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/style/tailwind.css';
+import { createI18n } from 'vue-i18n';
 
-createApp(App).mount('#app');
+const i18n = createI18n({
+    locale: 'en',
+    messages: {
+        en: {
+            message: {
+                language: 'Language',
+                hello: 'hello world!'
+            }
+        },
+        es: {
+            message: {
+                language: 'Lenguaje',
+                hello: 'Hola mundo'
+            }
+        }
+    }
+});
+
+const app = createApp(App);
+app.use(i18n).mount('#app');
