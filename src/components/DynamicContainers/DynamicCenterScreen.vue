c3524dfca161654b664782c76e217a74f90cac51
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-03-22 22:13:27 +0000

    fix css classes

--- src/components/DynamicContainers/DynamicCenterScreen.vue
+++ src/components/DynamicContainers/DynamicCenterScreen.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-full flex justify-center items center">
+    <div class="w-full h-full flex justify-center items center overflow-y-auto">
         <keep-alive>
             <component
                 :is="sectionToShow.name"
--- src/components/Sections/PackageSection.vue
+++ src/components/Sections/PackageSection.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="text-menu-color font-bold flex justify-start w-full h-full py-2 overflow-y-scroll"
+        class="text-menu-color font-bold flex justify-start w-full h-full py-2"
     >
         <pre v-html="info"></pre>
     </div>
