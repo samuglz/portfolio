5b7f95f60f10efa473f4920f94c9b57658bc2645
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-11-28 21:37:00 +0000

    fix: change header title

--- src/components/TopMenu/TopMenu.vue
+++ src/components/TopMenu/TopMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex w-full py-2 bg-background h-10">
         <div class="w-full text-center text-menu-color text-xs md:text-base">
-            Samuel González - Portfolio
+            Samuel González Alonso
         </div>
     </div>
 </template>
