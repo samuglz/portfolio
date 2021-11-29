f0bdbd91d13aa12f69f153bb157c2d2034314e23
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-11-29 15:36:46 +0000

    fix: indent

--- src/components/GitSection/GitSection.vue
+++ src/components/GitSection/GitSection.vue
@@ -1,56 +1,56 @@
 <template>
-  <div class="text-menu-color w-full overflow-y-auto">
-    <div
-      id="git"
-      class="grid grid-cols-12"
-    >
-      <GitTableHeader />
-      <template
-        v-for="(commit, index) in commits"
-        :key="index"
-      >
-        <GitTableInfo
-          :index="index"
-          :total-commits="commits.length"
-          :description="commit.description"
-          :date="commit.date"
-          :hash="commit.hash"
-          :type="commit.type"
-        />
-      </template>
+    <div class="text-menu-color w-full overflow-y-auto">
+        <div
+            id="git"
+            class="grid grid-cols-12"
+        >
+            <GitTableHeader />
+            <template
+                v-for="(commit, index) in commits"
+                :key="index"
+            >
+                <GitTableInfo
+                    :index="index"
+                    :total-commits="commits.length"
+                    :description="commit.description"
+                    :date="commit.date"
+                    :hash="commit.hash"
+                    :type="commit.type"
+                />
+            </template>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import { defineAsyncComponent, reactive, defineComponent } from 'vue'
-import { commitsInfo } from '../../data.js'
+import { defineAsyncComponent, reactive, defineComponent } from 'vue';
+import { commitsInfo } from '../../data.js';
 const GitTableHeader = defineAsyncComponent(() =>
-  import('./GitTableHeader.vue')
-)
-const GitTableInfo = defineAsyncComponent(() => import('./GitTableInfo.vue'))
+    import('./GitTableHeader.vue')
+);
+const GitTableInfo = defineAsyncComponent(() => import('./GitTableInfo.vue'));
 
 defineComponent({
-  name: 'GitSection'
-})
-const commits = reactive(commitsInfo.reverse())
+    name: 'GitSection'
+});
+const commits = reactive(commitsInfo.reverse());
 const digestMessage = async message => {
-  const msgUint8 = new TextEncoder().encode(message)
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8)
-  const hashArray = Array.from(new Uint8Array(hashBuffer))
-  const hashHex = hashArray
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('')
-  return hashHex
-}
+    const msgUint8 = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+        .map(b => b.toString(16).padStart(2, '0'))
+        .join('');
+    return hashHex;
+};
 const getHash = async text => {
-  const digestBuffer = await digestMessage(text)
-  return digestBuffer.substr(0, 8)
-}
+    const digestBuffer = await digestMessage(text);
+    return digestBuffer.substr(0, 8);
+};
 
 commits.forEach(async commit => {
-  commit.hash = await getHash(commit.description)
-})
+    commit.hash = await getHash(commit.description);
+});
 </script>
 
 <style scoped></style>
